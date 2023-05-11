5bfa07a74741fbdd512f7ee35e9862c78e42c0c2
Liam Muller <liam.w.muller@gmail.com>
2023-05-11 00:14:30 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "~/utils/api";
 import { Toaster } from "~/components/ui/toaster";
@@ -14,6 +15,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
       <Toaster />
     </SessionProvider>
   );
