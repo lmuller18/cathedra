3c60b35fa5ecef41b82ae533338c25aec2e7c195
Liam Muller <liam.w.muller@gmail.com>
2023-05-23 22:38:08 -0400

    fix build

--- src/elements/command.tsx
+++ src/elements/command.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import * as React from "react";
-import { type DialogProps } from "@radix-ui/react-dialog";
-import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
+import { Command as CommandPrimitive } from "cmdk";
+
+import type { DialogProps } from "@radix-ui/react-dialog";
 
 import { cn } from "~/lib/utils";
-import { Dialog, DialogContent } from "~/components/ui/dialog";
+
+import { Dialog, DialogContent } from "./dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
